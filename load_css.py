acb2df6bf963ca6939ee57533a592257db83123e
Anna Haensch <annahaensch@gmail.com>
2023-04-26 14:27:21 -0400

    add script to load css

---
+++ load_css.py
@@ -0,0 +1,8 @@
+"""
+Script to load css into streamlit app
+"""
+import streamlit as st
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
