e79c1e95b178907bb8128cee0113bd66643efa59
Anna Haensch <annahaensch@gmail.com>
2023-05-31 11:49:35 -0400

    update optimal left position paramters

--- optimal_left_position.py
+++ optimal_left_position.py
@@ -42,7 +42,7 @@ def main(L, R, alpha, beta):
     df["Density"] = pdf(x,model)
 
     # Select gamma for function input.
-    gamma = np.linspace(2.5, 3.5, 20)
+    gamma = np.linspace(1.0, 2.0, 15)
     final_ell_positions = []
     ell_dict = {}
     r_dict = {}
@@ -69,9 +69,9 @@ if __name__ == '__main__':
     """ Optional input arguments L, R, alpha, beta
     """
     L = -1
-    R = 2
+    R = 2.0
     alpha = 1
-    beta = 0.2
+    beta = 0
     if len(sys.argv) > 1:
         L, R, alpha, beta = sys.argv[1:]
     main(L, R, alpha, beta)
